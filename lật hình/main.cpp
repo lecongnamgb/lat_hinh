7c1dd4052d49566fabde1c7a2e00324cf126eab0
Le Cong Nam <lecongnam@gmail.com>
2020-05-26 14:59:01 +0700

    Initial Commit

---
+++ lật hình/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lật hình
+//
+//  Created by Lê Công Nam on 5/26/20.
+//  Copyright © 2020 Lê Công Nam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
